16c36c9059e1bd1dcd28efbbbfa46bd5a1245f4a
adupin <adupin@student.s19.be>
2023-09-26 16:36:48 +0200

    Revert "clean"
    This reverts commit d0fce40e2c6e80bd82905473c6938c59d7febe7a.

---
+++ minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adupin <adupin@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 13:29:21 by adupin            #+#    #+#             */
+/*   Updated: 2023/09/25 15:09:01 by adupin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>   // printf
+# include <stdlib.h>  // malloc, free, exit
+# include <unistd.h>  // access, open, read, close, fork, wait, waitpid, wait3, wait4, write, dup, execve, getcwd
+# include <signal.h>  // signal, sigaction, sigemptyset, sigaddset, kill
+# include <sys/stat.h> // stat, lstat, fstat
+# include <fcntl.h>   // open
+# include <dirent.h>  // opendir, readdir, closedir
+# include <string.h>  // strerror
+# include <errno.h>   // perror
+# include <unistd.h>  // isatty, ttyname, ttyslot, ioctl
+# include <stdlib.h>  // getenv
+# include <termios.h> // tcsetattr, tcgetattr
+# include <curses.h>  // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+
+#endif

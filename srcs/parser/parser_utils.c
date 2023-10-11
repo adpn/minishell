7a79065a34026016746050ec6a76a31fc6fd437b
Alexis Philippon <alexphil@mi-c2-p6.s19.be>
2023-10-11 16:09:31 +0200

    branch upd before merge

--- srcs/parser/parser_utils.c
+++ srcs/parser/parser_utils.c
@@ -6,11 +6,22 @@
 /*   By: alexphil <alexphil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 10:50:34 by alexphil          #+#    #+#             */
-/*   Updated: 2023/10/11 10:52:19 by alexphil         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:30:10 by alexphil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_parser_tools	init_parser_tools(t_lex *lex_list, t_tools *tools)
+{
+	t_parser_tools	parser_tools;
+
+	parser_tools.lexer_list = lex_list;
+	parser_tools.redirections = NULL;
+	parser_tools.num_redirections = 0;
+	parser_tools.tools = tools;
+	return (parser_tools);
+}
 
 void	count_pipes(t_lex *node, t_tools *tools)
 {
@@ -30,20 +41,16 @@ int	count_args(t_lex *node)
 	i = 0;
 	while (node && node->operator != PIPE)
 	{
-		if (!node->operator)
+		if (node->operator == WORD)
 			i++;
 		node = node->next;
 	}
 	return (i);
 }
 
-t_parser_tools	init_parser_tools(t_lex *lex_list, t_tools *tools)
+t_lex	*get_next_cmd(t_lex *node)
 {
-	t_parser_tools	parser_tools;
-
-	parser_tools.lexer_list = lex_list;
-	parser_tools.redirections = NULL;
-	parser_tools.num_redirections = 0;
-	parser_tools.tools = tools;
-	return (parser_tools);
+	while (node && node->operator != PIPE)
+		node = node->next;
+	return (node);
 }

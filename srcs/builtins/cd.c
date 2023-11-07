c6d05d2a58ccce3db55046cc13cf175dcd392ba1
adupin <adupin@student.s19.be>
2023-11-07 10:35:09 +0100

    au cas ou mais on s'en branle

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,97 +6,138 @@
 /*   By: adupin <adupin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:18:05 by adupin            #+#    #+#             */
-/*   Updated: 2023/11/02 14:10:02 by adupin           ###   ########.fr       */
+/*   Updated: 2023/11/07 10:34:48 by adupin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-char	*home_path(char *path)
-{
-	char	*home;
-	char	*new;
-
-	home = value_var_environ("HOME");
-	if (!home)
-	{
-		if (!path[0])
-			return (NULL);
-		return (ft_xstrdup(path));
-	}
-	if (!path || !path[0])
-		return (ft_xstrdup(path));
-	new = ft_strjoin(home, path);
-	if (!new)
-		return (NULL);
-	return (new);
-}
-
-void	update_pwd(t_tools *tools, char *new)
+void	update_pwd(t_tools *tools, char *new, char *input)
 {
 	char	*new_m;
 
 	if (!new)
 		return ;
-	new_m = ft_xstrdup(new);
-	if (chdir(new_m) == -1)
+	printf("new = %s\n", new);
+	if (chdir(new) == -1)
 	{
 		ft_putstr_fd("bash: cd: ", 2);
-		ft_putstr_fd(new_m, 2);
+		ft_putstr_fd(input, 2);
 		ft_putstr_fd(": No such file or directory\n", 2);
 		return ;
 	}
+	new_m = ft_xstrdup(new);
 	free(tools->old_pwd);
 	tools->old_pwd = tools->pwd;
 	replace_element_to_environ("OLDPWD", tools->old_pwd);
 	replace_element_to_environ("PWD", new_m);
 	tools->pwd = new_m;
 }
+// char	*pp_to_str(char **tab, char *c)
+// {
+// 	char *str;
+// 	int	size;
+// 	int	i;
 
-char	*get_absolute_path(t_tools *tools, char *str)
-{
-	char	*path;
-	char	*new;
-	int		i;
-	int		j;
+// 	i = 0;
+// 	size = 0;
+// 	while (tab[i])
+// 	{
+// 		size += ft_strlen(tab[i]) + 1;
+// 		i++;
+// 	}
+// 	str = ft_xmalloc(sizeof(char) * size + 1);
+// 	i = 0;
+// 	while (tab[i])
+// 	{
+// 		printf("tab = %s\n",tab[i]);
+// 		ft_strlcat(str, tab[i], size + 1);
+// 		ft_strlcat(str, c, size + 1);
+// 		free(tab[i]);
+// 		i++;
+// 	}
+// 	free(tab);
+// 	str[size - 1] = '\0';
+// 	return (str);
+// }
 
-	path = ft_xmalloc(ft_strlen(str) * sizeof(char) + 2);
-	i = 0;
-	j = 0;
-	while (str[i] && str[i] != '=')
-	{
-		if (str[i] != '\"' && str[i] != '\'')
-			path[j++] = str[i];
-		i++;
-	}
-	path[j] = '\0';
-	if (path[0] == '~') 
-	{
-		new = home_path(path + 1);
-		free(path);
-		return (new);
-	}
-	new = ft_strjoin(tools->pwd, path);
-	free(path);
-	return(new);
-}
+// void	remove_back(char **tab)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (tab[i])
+// 	{
+// 		if (!ft_strncmp("..", tab[i], ft_strlen(tab[i])))
+// 		{
+// 			free(tab[i]);
+// 			free(tab[i - 1]);
+// 			tab[i - 1] = NULL;
+// 			ft_ppcpy(&tab[i - 1], &tab[i + 1]);			
+// 		}
+// 		else
+// 			i++;
+// 	}	
+// }
+// char	*get_absolute_path(char *input, t_tools *tools)
+// {
+// 	char	*path;
+// 	char	**tmp;
+// 	char	**s_input;
+// 	char	**absolute;
+
+// 	if (input[0] == '~')
+// 	{
+// 		if (value_var_environ("HOME"))
+// 			tmp = ft_split(value_var_environ("HOME"), '/');
+// 		else
+// 			tmp = ft_xmalloc(sizeof(char));
+// 	}
+// 	else
+// 		tmp = ft_split(tools->pwd, '/');
+// 	if (!tmp)
+// 		return (NULL);
+// 	s_input = ft_split(input, '/');
+// 	if (!s_input)
+// 		return (NULL);
+// 	absolute = ft_xmalloc((ft_pplen(tmp) + ft_pplen(s_input) + 1) * sizeof(char *));
+// 	absolute[0] = NULL;
+// 	ft_ppcat(absolute, tmp);
+// 	ft_ppcat(absolute, s_input);
+// 	printf("absolute = %s\n", absolute[0]);
+// 	free(tmp);
+// 	free(s_input);
+// 	remove_back(absolute);
+// 	printf("absolute2 = %s\n", absolute[0]);
+// 	path = pp_to_str(absolute, "/");
+// 	printf("path = %s\n", path);
+// 	return (path);
+// }
 
 void	ft_cd(t_tools *tools, t_cmds *cmds)
 {
 	char	*path;
+	//CHDIR GERE .. TOUT SEUL ET LES CHEMINS RELATIFS
 	
-	if (!cmds->args[1])
-	{
-		update_pwd(tools, get_absolute_path(tools, "~"));
-		return ;
-	}
 	if (cmds->args[2])
 	{
-		ft_putendl_fd("bash: cd: too many arguments", 2);
+		ft_putstr_fd("bash: cd: too many arguments\n", 2);
 		return ;
 	}
-	path = get_absolute_path(tools, cmds->args[1]);
-	update_pwd(tools, path);
-	free(path);
-	//Need to update PWD and OLD, in environ and in struct
+	if (!ft_strncmp(cmds->args[1], "-", ft_strlen(cmds->args[1])))
+	{
+		if (!value_var_environ("OLDPWD"))
+		{
+			ft_putstr_fd("bash: cd: OLDPWD not set\n", 2);
+			return ;
+		}
+		update_pwd(tools, value_var_environ("OLDPWD"), NULL);
+		ft_putendl_fd(tools->pwd, 1);
+	}
+	else
+	{
+		path = get_absolute_path(cmds->args[1], tools);
+		update_pwd(tools, path, cmds->args[1]);
+		free(path);
+	}
 }
--- srcs/utils/pp.c
+++ srcs/utils/pp.c
@@ -1,31 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.h                                         :+:      :+:    :+:   */
+/*   pp.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adupin <adupin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/28 11:16:17 by adupin            #+#    #+#             */
-/*   Updated: 2023/11/03 11:59:16 by adupin           ###   ########.fr       */
+/*   Created: 2023/11/06 12:33:21 by adupin            #+#    #+#             */
+/*   Updated: 2023/11/06 15:59:29 by adupin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTINS_H
-# define BUILTINS_H
-
-#include "minishell.h"
-#include "struct.h"
 #include "utils.h"
 
-typedef struct	s_var_env
+size_t	ft_pplen(char **tab)
 {
-	char	*name;
-	char	*value;
-}				t_var_env;
+	size_t	i;
 
-void	builtin(t_tools *tools, t_cmds *cmds);
-void	ms_echo(t_tools *tools, t_cmds *cmd);
-void	ft_export(t_tools *tools, t_cmds *cmds);
-void	ft_cd(t_tools *tools, t_cmds *cmds);
-void	push_var_to_environ(t_var_env *var_env);
-#endif
+	i = 0;
+	while (tab[i])
+		i++;
+	return (i);
+}
+
+void	ft_ppcat(char **dst, char **src)
+{
+	size_t	i;
+	size_t	j;
+	
+	i = 0;
+	j = 0;
+	while (dst[i])
+		i++;
+	while (src[j])
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = NULL;
+}
+
+void	ft_ppcpy(char **dst, char **src)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = NULL;
+}

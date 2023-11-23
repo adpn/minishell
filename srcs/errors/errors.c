2dfef27de4ef445191ee87288eef118c18a2c7b9
Alexis Philippon <alexphil@mi-c2-p7.s19.be>
2023-11-23 10:45:18 +0100

    Adding some error management

--- srcs/errors/errors.c
+++ srcs/errors/errors.c
@@ -6,16 +6,35 @@
 /*   By: alexphil <alexphil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 09:50:38 by alexphil          #+#    #+#             */
-/*   Updated: 2023/11/21 09:56:52 by alexphil         ###   ########.fr       */
+/*   Updated: 2023/11/23 10:36:12 by alexphil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "errors.h"
 
-int	cmd_not_found(char *nope)
+int	parser_token_error(t_tools *tools, int operator)
+{
+	ft_putstr_fd("minishell: syntax error near unexpected token ",
+		STDERR_FILENO);
+	if (operator == PIPE)
+		ft_putstr_fd("'|'\n", STDERR_FILENO);
+	else if (operator == HEREDOC)
+		ft_putstr_fd("'<<'\n", STDERR_FILENO);
+	else if (operator == R_APP)
+		ft_putstr_fd("'>>'\n", STDERR_FILENO);
+	else if (operator == R_INPUT)
+		ft_putstr_fd("'<'\n", STDERR_FILENO);
+	else if (operator == R_OUTPUT)
+		ft_putstr_fd("'>'\n", STDERR_FILENO);
+	return (EXIT_FAILURE);
+}
+
+void	error_mgmt(t_tools *tools, int code)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
-	ft_putstr_fd(nope, STDERR_FILENO);
-	ft_putstr_fd(": command not found\n", STDERR_FILENO);
-	return (127);
+	if (code == 1)
+		ft_putstr_fd("failure to create pipe\n", STDERR_FILENO);
+	if (code == 2)
+		ft_putstr_fd("failure to fork process\n", STDERR_FILENO);
+	resetools(tools);
 }

16ead5c7025cb56f1951639708639491159f67b2
Augustin <augustin.dupin27@gmail.com>
2023-12-03 00:43:45 +0100

    Expand now stop doing in an infinite loop, need to check when to stop searching name, supposedly handles single quotes

--- srcs/expander/expander.c
+++ srcs/expander/expander.c
@@ -12,15 +12,46 @@
 
 #include "expander.h"
 
-char	*copy_new_string(char *str, char *name, char *value, char *dol)
+static int	len_total(char **tab)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (tab[i])
+	{
+		len += ft_strlen(tab[i]);
+		i++;
+	}
+	return (len);
+}
+
+char	*cat_all_in_one(char **tab)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = ft_xcalloc((len_total(tab) + 1) * sizeof(char));
+	while (tab[i])
+	{
+		ft_strlcat(str, tab[i], len_total(tab) + 2);
+		ft_strlcat(str, "", len_total(tab) + 2);
+		i++;
+	}
+	return (str);
+}
+
+char	*copy_new_string(char *str, char *name, char *value)
 {
 	char	*new;
 
 	new = ft_xmalloc(ft_strlen(str) - ft_strlen(name) + ft_strlen(value) + 1);
-	ft_strlcpy(new, str, (dol - str) + 1);
+	ft_strlcpy(new, str, str + 1);
 	ft_strlcat(new, value, ft_strlen(new) + ft_strlen(value) + 1);
-	ft_strlcat(new, &str[(dol - str) + ft_strlen(name) + 1],
-		ft_strlen(new) + ft_strlen(&str[(dol - str) + ft_strlen(name)]) + 1);
+	ft_strlcat(new, &str[str + ft_strlen(name) + 1],
+		ft_strlen(new) + ft_strlen(&str[str + ft_strlen(name)]) + 1);
 	return (new);
 }
 
@@ -47,30 +78,92 @@ void	remove_empty_string(char **tab)
 	}
 }
 
-char	*complete_string(char *str, t_tools *tools)
+char	*replace_dollar(char *str, t_tools *tools)
 {
 	char	*new;
 	char	*value;
 	char	*name;
-	char	*dol;
 
-	while (get_dollar(str))
-	{
-		dol = get_dollar(str);
-		name = get_var_name(dol);
-		if (!name)
-			return (NULL);
-		value = get_value(name, tools);
-		if (!value)
-			return (free(name), NULL);
-		new = copy_new_string(str, name, value, dol);
-		if (name[0] == '?')
-			free(value);
-		free(str);
-		free(name);
-		str = new;
-	}
+	name = get_var_name(str);
+	if (!name)
+		return (NULL);
+	value = get_value(name, tools);
+	if (!value)
+		return (free(name), NULL);
+	new = copy_new_string(str, name, value);
+	if (name[0] == '?')
+		free(value);
+	free(str);
+	free(name);
+	str = new;
 	return (str);
+}
+
+int	ft_count(char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count += 1;
+		i++;
+	}
+	return (count);
+}
+
+char	**split_on_dollar(char *str)
+{
+	char	**tab;
+	int		i;
+	int		j;
+	t_quotes	quotes;
+
+	tab = ft_xmalloc(sizeof(char *) * (ft_count(str, '$') + 2));
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		update_quotes(&quotes, str[i]);
+		if (str[i] == '$' && quotes.simple_q % 2 == 0)
+		{
+			tab[j] = ft_xmalloc(i + 1);
+			ft_strlcpy(tab[j], str, i + 1);
+			str += i;
+			j++;
+			i = 0;
+		}
+		i++;
+	}
+	tab[j] = ft_xmalloc(i + 1);
+	ft_strlcpy(tab[j], str, i + 1);
+	tab[j + 1] = NULL;
+	return (tab);
+}
+
+char	*complete_clean(char *str, t_tools *tools)
+{
+	char		**tab;
+	int			i;
+	char 		*new;
+
+	tab = split_on_dollar(str);
+	i = 0;
+	while (tab[i])
+	{
+		if (tab[i][0] == '$')
+		{
+			tab[i] = replace_dollar(tab[i], tools);
+		}
+		i++;
+	}
+	new = cat_all_in_one(tab);
+	ft_free_split(tab);
+	free(str);
+	return (new);
 }
 
 //Return 1 if success, 0 if malloc failed
@@ -81,12 +174,7 @@ int	expand(char **tab, t_tools *tools)
 	i = 0;
 	while (tab[i])
 	{
-		if (get_dollar(tab[i]))
-		{
-			tab[i] = complete_string(tab[i], tools);
-			if (!tab[i])
-				return (0);
-		}
+		tab[i] = complete_clean(tab[i], tools);
 		i++;
 	}
 	remove_empty_string(tab);
